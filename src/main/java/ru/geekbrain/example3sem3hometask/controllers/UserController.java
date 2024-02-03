61f8ef3a59c595fb6326db91aba0909255962788
StephYa7 <1000p@mail.ru>
2024-02-04 02:27:26 +0700

    1 added @ModelAttribute in usrerAddFromParm instead of RequestParm

--- src/main/java/ru/geekbrain/example3sem3hometask/controllers/UserController.java
+++ src/main/java/ru/geekbrain/example3sem3hometask/controllers/UserController.java
@@ -31,16 +31,22 @@ public class UserController {
     }
 
     @PostMapping()
-    public String userAddFromParam(@RequestParam("name") String name,
-                                   @RequestParam("age") int age,
-                                   @RequestParam("email") String email) {
+    public String userAddFromParam(
+//            @RequestParam("name") String name,
+//            @RequestParam("age") int age,
+//            @RequestParam("email") String email,
+            @ModelAttribute("user") User user
+    ) {
 //        User newUser = new User();
 //        newUser.setName(name);
 //        newUser.setAge(age);
 //        newUser.setEmail(email);
 //        registrationService.getDataProcessingService().getRepository().save(user);
 
-        registrationService.processRegistration(name, age, email);
+//        registrationService.processRegistration(name, age, email);
+        registrationService.processRegistration(user.getName(), user.getAge(), user.getEmail());
+        // Вообще все это выглядит очень сомнительно. Передавать уже созданного юзера
+        // в методы создания и нотификации, возможно еще придумаю что-то более адекватное до проверки.
         return "user added";
     }
 }
