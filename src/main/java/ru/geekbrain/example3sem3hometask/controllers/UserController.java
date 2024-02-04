2281b5f4b58e362528e8cd49784a3ca9480454e8
StephYa7 <1000p@mail.ru>
2024-02-04 14:13:05 +0700

    cleaned up code

--- src/main/java/ru/geekbrain/example3sem3hometask/controllers/UserController.java
+++ src/main/java/ru/geekbrain/example3sem3hometask/controllers/UserController.java
@@ -23,8 +23,6 @@ public class UserController {
         if (user == null) {
             throw new IllegalArgumentException();
         }
-//        registrationService.getDataProcessingService().getRepository().save(user);
-
         registrationService.processRegistration(user.getName(), user.getAge(), user.getEmail());
 
         return "user added";
@@ -32,21 +30,10 @@ public class UserController {
 
     @PostMapping()
     public String userAddFromParam(
-//            @RequestParam("name") String name,
-//            @RequestParam("age") int age,
-//            @RequestParam("email") String email,
             @ModelAttribute("user") User user
     ) {
-//        User newUser = new User();
-//        newUser.setName(name);
-//        newUser.setAge(age);
-//        newUser.setEmail(email);
-//        registrationService.getDataProcessingService().getRepository().save(user);
-
-//        registrationService.processRegistration(name, age, email);
         registrationService.processRegistration(user.getName(), user.getAge(), user.getEmail());
-        // Вообще все это выглядит очень сомнительно. Передавать уже созданного юзера
-        // в методы создания и нотификации, возможно еще придумаю что-то более адекватное до проверки.
+
         return "user added";
     }
 }
